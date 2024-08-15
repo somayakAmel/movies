64df4fc2dcfeeeff519ccec8755aea3ee242a056
somayakAmel <ahagav10@gmail.com>
2024-08-15 11:28:01 +0300

    first commit

---
+++ lib/src/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
